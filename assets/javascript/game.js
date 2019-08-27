5c5234559aaa094a1b948a51bbc4d12c82a52ab5
Rachael Clark <rachael.clark@uconn.edu>
2019-08-27 19:55:56 -0400

    Layout and CSS to match portfolio

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+var randomnumber = Math.floor(Math.random() * (121-19)) + 19;
+        
+console.log(randomnumber);
+
+
+//1. Display random number on screen:
+
+//2. Assign random numbers to crystals
+    //Values should be between 1-12
+    //Valuse of crystals change each round
+
+//3. Add values upon each click
+
+//4. Complete Game
+    //Win: Value is equal to randumb number
+    //Lose: Value is greater than random number
+
+//5. Reset Game
+        
